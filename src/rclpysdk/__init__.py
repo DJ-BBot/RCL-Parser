95d7de3e4879fc2bb919d2acdd01aba97b73ba23
Roger Barker <pendletonroger@gmail.com>
2024-03-15 01:07:49 -0500

    Updated RCL parser to have an initial state if instantiated

---
+++ src/rclpysdk/__init__.py
@@ -0,0 +1,1 @@
+from .rclsdk import rclParser
--- src/rclpysdk/rclsdk.py
+++ src/rclpysdk/rclsdk.py
@@ -1,4 +1,6 @@
-class rcl:
+import os
+
+class rclParser:
     """RCL is Roger's Configuration Language. This class is the pythonic parser sdk."""
     
     __tokens:list[str] = [
@@ -7,9 +9,31 @@ class rcl:
         "{", "}", ".", ";"
     ]
 
+    __keywords:list[str] = [
+        "title", "rclAuthor", "rclVersion"
+    ]
+
     @classmethod
-    def __GetRCLTokens(cls):
+    def __GetRCLTokens(cls) -> list[str]:
         return cls.__tokens
+    
+    @classmethod
+    def __GetRCLKeywords(cls) -> list[str]:
+        return cls.__keywords
+    
+    def __init__(self):
+        # initialize some values
+        self.sourceFile:os.DirEntry = None
+        self.className:str = ""
+        self.classLanguage:str = ""
+        self.rclAsJson:str = ""
+        self.rclAsString:str = ""
+        self.rclAsDict:dict[str,str] = {}
+        self.rclLines = []
+        self.genTargetClass:bool = False
+        self.genTargetAsJSON:bool = False
+        self.genTargetAsStr:bool = False
+        self.genTargetAsDict:bool = False
 
 if __name__ == "__main__":
-    print(rcl)
+    print(rclParser)
